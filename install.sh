70c16a45c3917e3f22b277b25c60c40d024c998f
Theo Godin <210170@umons.ac.be>
2022-12-06 18:33:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 mkdir -p $HOME/bin
-cp bashquote.sh $HOME/bin/
-chmod +x $HOME/bin/bashquote.sh
+cp bashquote $HOME/bin/
+chmod +x $HOME/bin/bashquote
 mkdir -p $HOME/.config/bashquote
-cp quotes.txt $HOME/.config/bashquote/
-bashrc=$(grep "bashquote" $HOME/.bashrc)
-if [ -n $bashrc ];
-then
-    echo "# bashquote when terminal opened" >> $HOME/.bashrc
-    echo "./bin/bashquote.sh" >> $HOME/.bashrc
-    . $HOME/.bashrc
-fi
+cp quotes/* $HOME/.config/bashquote/
 echo "Installation completed"
