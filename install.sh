3a29301526fbf9ad162a9a8e07d03bad7914c1c6
VionFrancois <78816256+VionFrancois@users.noreply.github.com>
2022-12-06 17:32:27 +0100

    Renamed in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mkdir -p $HOME/bin
-cp biblequote.sh $HOME/bin/
-chmod +x $HOME/bin/biblequote.sh
-mkdir -p $HOME/.config/biblequote
-cp quotes.txt $HOME/.config/biblequote/
-bashrc=$(grep "biblequote" $HOME/.bashrc)
+cp bashquote.sh $HOME/bin/
+chmod +x $HOME/bin/bashquote.sh
+mkdir -p $HOME/.config/bashquote
+cp quotes.txt $HOME/.config/bashquote/
+bashrc=$(grep "bashquote" $HOME/.bashrc)
 if [ -n $bashrc ];
 then
-    echo "# biblequote when terminal opened" >> $HOME/.bashrc
-    echo "./bin/biblequote.sh" >> $HOME/.bashrc
+    echo "# bashquote when terminal opened" >> $HOME/.bashrc
+    echo "./bin/bashquote.sh" >> $HOME/.bashrc
     . $HOME/.bashrc
 fi
 echo "Installation completed"
