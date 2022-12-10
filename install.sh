b40b012c7f49370e3282a7cd366596d3a53dd59b
Theo <theo.godin@student.umons.ac.be>
2022-12-10 13:11:20 +0100

    added custom symbols support

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mkdir -p $HOME/bin
-cp bashquote $HOME/bin/
-chmod +x $HOME/bin/bashquote
+cp bashquote $HOME/.local/bin/
+chmod +x $HOME/.local/bin/bashquote
 mkdir -p $HOME/.config/bashquote
 cp quotes/* $HOME/.config/bashquote/
 echo "Installation completed"
